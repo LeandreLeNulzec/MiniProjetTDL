7b195732583e0657db68e4b493b0f153c72fe4c8
LE NULZEC Leandre <llc0251@winterfell.enseeiht.fr>
2025-05-13 22:30:52 +0200

    avancées

--- Mini-projet/fr.n7.stl.minic/src/fr/n7/stl/minic/ast/expression/accessible/AddressAccess.java
+++ Mini-projet/fr.n7.stl.minic/src/fr/n7/stl/minic/ast/expression/accessible/AddressAccess.java
@@ -7,6 +7,7 @@ import fr.n7.stl.minic.ast.SemanticsUndefinedException;
 import fr.n7.stl.minic.ast.expression.assignable.AssignableExpression;
 import fr.n7.stl.minic.ast.scope.Declaration;
 import fr.n7.stl.minic.ast.scope.HierarchicalScope;
+import fr.n7.stl.minic.ast.type.PointerType;
 import fr.n7.stl.minic.ast.type.Type;
 import fr.n7.stl.tam.ast.Fragment;
 import fr.n7.stl.tam.ast.TAMFactory;
@@ -34,7 +35,7 @@ public class AddressAccess implements AccessibleExpression {
 	 */
 	@Override
 	public boolean collectAndPartialResolve(HierarchicalScope<Declaration> _scope) {
-		throw new SemanticsUndefinedException( "collect is undefined in AddressAccess.");	
+		return this.assignable.collectAndPartialResolve(_scope);	
 	}
 
 	/* (non-Javadoc)
@@ -42,7 +43,7 @@ public class AddressAccess implements AccessibleExpression {
 	 */
 	@Override
 	public boolean completeResolve(HierarchicalScope<Declaration> _scope) {
-		throw new SemanticsUndefinedException( "resolve is undefined in AddressAccess.");	
+		return true;
 	}
 	
 	/* (non-Javadoc)
@@ -50,7 +51,7 @@ public class AddressAccess implements AccessibleExpression {
 	 */
 	@Override
 	public Type getType() {
-		throw new SemanticsUndefinedException( "getType is undefined in AddressAccess.");
+		return new PointerType(this.assignable.getType());
 	}
 	
 	/* (non-Javadoc)
